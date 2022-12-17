84191b448ab9f5e4aa5ea368253c7b4d6175587c
d <d>
2022-12-17 11:19:41 +0300

    feat: update commen.js and steam.api

--- steamAPI.js
+++ steamAPI.js
@@ -25,19 +25,42 @@ async function showData() {
     let totalkills = stats[0].value;
     let totaldeaths = stats[1].value;
     let kdration = totalkills / totaldeaths;
+    let totalwins = stats[5].value;
+    let totalwinspistolround = stats[27].value;
+    let lastmatchdamage = stats[98].value;
+    let totalkillselite = stats[13].value;
+    let totalmvps = stats[102].value;
+    let totalhitsp250 = stats[139].value;
+    let totalkillsnova = stats[158].value;
+    let totalkillsawp = stats[19].value;
+    let totalkillsak47 = stats[20].value;
+    let totalkillsaug = stats[21].value;
+    let totalkillsfamas = stats[22].value;
     let kd = kdration.toFixed(2)
     console.log(kdration);
     let playerBox = `
-        <a href="#"  class="news-wall-image">
-                <img alt="Europe" src="preview/EU1.gif" class="news-flag flag" title="Europe">
-                <div class="news-text">kd лучшего игрока в кс го ${kd}</div>
-                <div class="news-tc">
-                    <div class="news-recent">10 минут назад</div>
-                    <div>45 комментариев</div>
-                </div>
-            </a>
+        <div class="columns">
+    <div class="col stats-rows standard-box">
+      <div class="stats-row"><span>K/D Ratio</span><span>${kd}</span></div>
+      <div class="stats-row"><span>Total kills</span><span>${totalkills}</span></div>
+      <div class="stats-row"><span>Total deaths</span><span>${totaldeaths}</span></div>
+      <div class="stats-row"><span>Total wins</span><span>${totalwins}</span></div>
+      <div class="stats-row"><span>total wins round</span><span>${totalwinspistolround}</span></div>
+      <div class="stats-row"><span>match damage</span><span>${lastmatchdamage}</span></div>
+      <div class="stats-row"><span>total kills elite</span><span>${totalkillselite}</span></div>
+    </div>
+    <div class="col stats-rows standard-box">
+      <div class="stats-row"><span>total mvps</span><span>${totalmvps}</span></div>
+      <div class="stats-row"><span class="">totalhitsp250</span><span>${totalhitsp250}</span></div>
+      <div class="stats-row"><span class="">total kills nova</span><span>${totalkillsnova}</span></div>
+      <div class="stats-row"><span class="">total kills awp</span><span>${totalkillsawp}</span></div>
+      <div class="stats-row"><span>total kills ak47</span><span>${totalkillsak47}</span></div>
+      <div class="stats-row"><span>total kills aug</span><span>${totalkillsaug}</span></div>
+      <div class="stats-row"><span>total kills famas</span><span>${totalkillsfamas}</span></div>
+    </div>
+  </div>
         `;
-    document.querySelector(".news-wall").insertAdjacentHTML("afterbegin", playerBox);
+    document.querySelector(".statistics").insertAdjacentHTML("beforeend", playerBox);
 }
 
 async function pageLoaded() {
